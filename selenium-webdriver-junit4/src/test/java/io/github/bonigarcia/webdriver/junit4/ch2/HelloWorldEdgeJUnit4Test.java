d0f1847b6fa11cfb46a20cc0c6aefac09f2e7db7
Boni García <boni.garcia@uc3m.es>
2021-06-08 10:41:46 +0200

    Include hello world test using Edge as browser

---
+++ selenium-webdriver-junit4/src/test/java/io/github/bonigarcia/webdriver/junit4/ch2/HelloWorldEdgeJUnit4Test.java
@@ -0,0 +1,68 @@
+/*
+ * (C) Copyright 2021 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.webdriver.junit4.ch2;
+
+import static com.google.common.truth.Truth.assertThat;
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.slf4j.Logger;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class HelloWorldEdgeJUnit4Test {
+
+    static final Logger log = getLogger(lookup().lookupClass());
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void setupClass() {
+        WebDriverManager.edgedriver().setup();
+    }
+
+    @Before
+    public void setup() {
+        driver = new EdgeDriver();
+    }
+
+    @After
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void test() {
+        // Exercise
+        String sutUrl = "https://bonigarcia.github.io/selenium-webdriver-java/";
+        driver.get(sutUrl);
+        String title = driver.getTitle();
+        log.debug("The title of {} is {}", sutUrl, title);
+
+        // Verify
+        assertThat(title).isEqualTo("Hands-on Selenium WebDriver with Java");
+    }
+
+}
---
+++ selenium-webdriver-testng/src/test/java/io/github/bonigarcia/webdriver/testng/ch2/HelloWorldEdgeTestNGTest.java
@@ -0,0 +1,68 @@
+/*
+ * (C) Copyright 2021 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.webdriver.testng.ch2;
+
+import static com.google.common.truth.Truth.assertThat;
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.slf4j.Logger;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class HelloWorldEdgeTestNGTest {
+
+    static final Logger log = getLogger(lookup().lookupClass());
+
+    private WebDriver driver;
+
+    @BeforeSuite
+    public void setupSuite() {
+        WebDriverManager.edgedriver().setup();
+    }
+
+    @BeforeTest
+    public void setup() {
+        driver = new EdgeDriver();
+    }
+
+    @AfterTest
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void test() {
+        // Exercise
+        String sutUrl = "https://bonigarcia.github.io/selenium-webdriver-java/";
+        driver.get(sutUrl);
+        String title = driver.getTitle();
+        log.debug("The title of {} is {}", sutUrl, title);
+
+        // Verify
+        assertThat(title).isEqualTo("Hands-on Selenium WebDriver with Java");
+    }
+
+}
