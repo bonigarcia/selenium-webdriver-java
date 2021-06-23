250fbf7ccaeb39a3f2f8af4053cafa7de97803b8
Boni García <boni.garcia@uc3m.es>
2021-06-23 11:45:31 +0200

    Include test skeletons

---
+++ selenium-webdriver-junit4/src/test/java/io/github/bonigarcia/webdriver/junit4/ch2/skeletons/SkeletonEdgeJUnit4Test.java
@@ -0,0 +1,54 @@
+/*
+ * (C) Copyright 2021 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.webdriver.junit4.ch2.skeletons;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SkeletonEdgeJUnit4Test {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void setupClass() {
+        WebDriverManager.edgedriver().setup();
+    }
+
+    @Before
+    public void setup() {
+        driver = new EdgeDriver();
+    }
+
+    @After
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void test() {
+        // TODO: use variable "webdriver" to call Selenium WebDriver API
+    }
+
+}
---
+++ selenium-webdriver-testng/src/test/java/io/github/bonigarcia/webdriver/testng/ch2/skeletons/SkeletonFirefoxTestNGTest.java
@@ -0,0 +1,54 @@
+/*
+ * (C) Copyright 2021 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.webdriver.testng.ch2.skeletons;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SkeletonFirefoxTestNGTest {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setupClass() {
+        WebDriverManager.firefoxdriver().setup();
+    }
+
+    @BeforeMethod
+    public void setup() {
+        driver = new FirefoxDriver();
+    }
+
+    @AfterMethod
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void test() {
+        // TODO: use variable "webdriver" to call Selenium WebDriver API
+    }
+
+}
