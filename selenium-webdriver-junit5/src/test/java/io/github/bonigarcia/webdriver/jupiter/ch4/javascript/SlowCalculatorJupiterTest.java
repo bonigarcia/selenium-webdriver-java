a068ad4d11da9f8e8fbfa2631f2dcef4868682e5
Boni García <boni.garcia@uc3m.es>
2021-08-17 18:04:25 +0200

    Revert "Include explicit wait condition in slow calculator test"
    This reverts commit 4ce54de52f346d94573b75d13ff6f671a7bb3508.

--- selenium-webdriver-junit5/src/test/java/io/github/bonigarcia/webdriver/jupiter/ch4/javascript/SlowCalculatorJupiterTest.java
+++ selenium-webdriver-junit5/src/test/java/io/github/bonigarcia/webdriver/jupiter/ch4/javascript/SlowCalculatorJupiterTest.java
@@ -22,6 +22,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -62,9 +63,8 @@ class SlowCalculatorJupiterTest {
 
     void clickElement(String xpath) {
         WebElement element = driver.findElement(By.xpath(xpath));
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.elementToBeClickable(element));
-        element.click();
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();",
+                element);
     }
 
 }
