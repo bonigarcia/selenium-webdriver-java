3162e4dc8d99a37805f572780b3bbeb3f9ed98d7
Boni García <boni.garcia@uc3m.es>
2021-08-23 21:21:38 +0200

    Include chapter 4 tests using Selenium-Jupiter

---
+++ selenium-webdriver-junit5-seljup/src/test/java/io/github/bonigarcia/webdriver/seljup/ch4/event_listeners/EventListenerSelJupTest.java
@@ -0,0 +1,54 @@
+/*
+ * (C) Copyright 2021 Boni Garcia (https://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.webdriver.seljup.ch4.event_listeners;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringDecorator;
+
+import io.github.bonigarcia.seljup.SeleniumJupiter;
+
+@ExtendWith(SeleniumJupiter.class)
+class EventListenerSelJupTest {
+
+    WebDriver driver;
+
+    public EventListenerSelJupTest(ChromeDriver driver) {
+        MyEventListener listener = new MyEventListener();
+        this.driver = new EventFiringDecorator(listener).decorate(driver);
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    void testEventListener() {
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+        assertThat(driver.getTitle())
+                .isEqualTo("Hands-on Selenium WebDriver with Java");
+        driver.findElement(By.linkText("Web form")).click();
+    }
+
+}
