5cf09cf358818c705d34e279e84167d2baae0ee0
Boni García <boni.garcia@uc3m.es>
2021-08-31 15:52:19 +0200

    Remove unused import in test

--- selenium-webdriver-junit5/src/test/java/io/github/bonigarcia/webdriver/jupiter/ch5/caps/geolocation/GeolocationEdgeJupiterTest.java
+++ selenium-webdriver-junit5/src/test/java/io/github/bonigarcia/webdriver/jupiter/ch5/caps/geolocation/GeolocationEdgeJupiterTest.java
@@ -25,7 +25,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
