86ff9b65fc9329b6a51caae0703b770ffb23e8af
Boni García <boni.garcia@uc3m.es>
2021-10-27 19:22:19 +0200

    Include tests for chapter 5 and 6 using Selenium-Jupiter

---
+++ selenium-webdriver-junit5-seljup/src/test/java/io/github/bonigarcia/webdriver/seljup/ch5/shadow_dom/ShadowDomSelJupTest.java
@@ -0,0 +1,49 @@
+/*
+ * (C) Copyright 2021 Boni Garcia (https://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.webdriver.seljup.ch5.shadow_dom;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.seljup.SeleniumJupiter;
+
+@Disabled
+@ExtendWith(SeleniumJupiter.class)
+class ShadowDomSelJupTest {
+
+    WebDriver driver;
+
+    @Test
+    void testShadowDom(ChromeDriver driver) {
+        driver.get(
+                "https://bonigarcia.dev/selenium-webdriver-java/shadow-dom.html");
+
+        WebElement content = driver.findElement(By.id("content"));
+        SearchContext shadowRoot = content.getShadowRoot();
+        WebElement textElement = shadowRoot.findElement(By.cssSelector("p"));
+        assertThat(textElement.getText()).contains("Hello Shadow DOM");
+    }
+
+}
