b04c536c1b0287c306858674c284b9e13accba58
Boni García <boni.garcia@uc3m.es>
2021-10-27 17:21:47 +0200

    Include tests for chapter 5 and 6 using TestNG

---
+++ selenium-webdriver-testng/src/test/java/io/github/bonigarcia/webdriver/testng/ch5/proxy/ProxyNGTest.java
@@ -0,0 +1,61 @@
+/*
+ * (C) Copyright 2021 Boni Garcia (https://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.webdriver.testng.ch5.proxy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ProxyNGTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        Proxy proxy = new Proxy();
+        String proxyStr = "proxy:port";
+        proxy.setHttpProxy(proxyStr);
+        proxy.setSslProxy(proxyStr);
+
+        ChromeOptions options = new ChromeOptions();
+        options.setCapability("acceptInsecureCerts", true);
+        options.setProxy(proxy);
+        // The previous line is equivalent to:
+        // options.setCapability(CapabilityType.PROXY, proxy);
+
+        driver = WebDriverManager.chromedriver().capabilities(options).create();
+    }
+
+    @AfterMethod
+    public void teardown() {
+        driver.quit();
+    }
+
+    @Test(enabled = false)
+    public void testProxy() {
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+        assertThat(driver.getTitle()).contains("Selenium WebDriver");
+    }
+
+}
