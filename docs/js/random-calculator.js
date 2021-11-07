24f6b4c3e8b4f6db4aee3b22e3778fca63966829
Boni García <boni.garcia@uc3m.es>
2021-11-07 17:34:16 +0100

    Change initial count value in random calculator JavaScript logic

--- docs/js/random-calculator.js
+++ docs/js/random-calculator.js
@@ -1,7 +1,7 @@
 var keys = document.querySelectorAll('#calculator span');
 var operators = ['+', '-', 'x', '÷'];
 var decimalAdded = false;
-var count = 0;
+var count = 1;
 
 for (var i = 0; i < keys.length; i++) {
     keys[i].onclick = function (e) {
