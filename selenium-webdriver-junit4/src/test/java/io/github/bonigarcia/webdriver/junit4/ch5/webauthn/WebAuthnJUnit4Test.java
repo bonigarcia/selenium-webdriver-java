274361051a9c4a98138a3e4952c5fd01e6977dd1
Boni García <boni.garcia@uc3m.es>
2021-11-09 17:35:37 +0100

    Reduce timeout in WebAuthn tests

--- selenium-webdriver-junit4/src/test/java/io/github/bonigarcia/webdriver/junit4/ch5/webauthn/WebAuthnJUnit4Test.java
+++ selenium-webdriver-junit4/src/test/java/io/github/bonigarcia/webdriver/junit4/ch5/webauthn/WebAuthnJUnit4Test.java
@@ -61,7 +61,7 @@ public class WebAuthnJUnit4Test {
         String randomId = UUID.randomUUID().toString();
         driver.findElement(By.id("input-email")).sendKeys(randomId);
         driver.findElement(By.id("register-button")).click();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         wait.until(ExpectedConditions.textToBePresentInElementLocated(
                 By.className("popover-body"), "Success! Now try logging in"));
 
--- selenium-webdriver-junit5/src/test/java/io/github/bonigarcia/webdriver/jupiter/ch5/webauthn/WebAuthnJupiterTest.java
+++ selenium-webdriver-junit5/src/test/java/io/github/bonigarcia/webdriver/jupiter/ch5/webauthn/WebAuthnJupiterTest.java
@@ -61,7 +61,7 @@ class WebAuthnJupiterTest {
         String randomId = UUID.randomUUID().toString();
         driver.findElement(By.id("input-email")).sendKeys(randomId);
         driver.findElement(By.id("register-button")).click();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         wait.until(ExpectedConditions.textToBePresentInElementLocated(
                 By.className("popover-body"), "Success! Now try logging in"));
 
