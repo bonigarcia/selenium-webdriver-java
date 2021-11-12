d97f6ea778b0515645b7a16d36142829a571b33b
Boni García <boni.garcia@uc3m.es>
2021-11-12 11:27:18 +0100

    Simplify custom runner for test listener in JUnit 4

--- selenium-webdriver-junit4/src/test/java/io/github/bonigarcia/webdriver/junit4/ch8/listeners/MyTestRunner.java
+++ selenium-webdriver-junit4/src/test/java/io/github/bonigarcia/webdriver/junit4/ch8/listeners/MyTestRunner.java
@@ -16,13 +16,9 @@
  */
 package io.github.bonigarcia.webdriver.junit4.ch8.listeners;
 
-import org.junit.AssumptionViolatedException;
-import org.junit.internal.runners.model.EachTestNotifier;
 import org.junit.runner.notification.RunNotifier;
-import org.junit.runner.notification.StoppedByUserException;
 import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.InitializationError;
-import org.junit.runners.model.Statement;
 
 public class MyTestRunner extends BlockJUnit4ClassRunner {
 
@@ -33,18 +29,6 @@ public class MyTestRunner extends BlockJUnit4ClassRunner {
     @Override
     public void run(RunNotifier notifier) {
         notifier.addListener(new MyTestListener());
-        EachTestNotifier testNotifier = new EachTestNotifier(notifier,
-                getDescription());
-        try {
-            notifier.fireTestRunStarted(getDescription());
-            Statement statement = classBlock(notifier);
-            statement.evaluate();
-        } catch (AssumptionViolatedException e) {
-            testNotifier.fireTestIgnored();
-        } catch (StoppedByUserException e) {
-            throw e;
-        } catch (Throwable e) {
-            testNotifier.addFailure(e);
-        }
+        super.run(notifier);
     }
 }
