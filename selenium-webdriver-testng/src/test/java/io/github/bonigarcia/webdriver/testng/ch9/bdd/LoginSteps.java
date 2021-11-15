224204d6ffa527a0b598ae4dffaf45881f883568
Boni García <boni.garcia@uc3m.es>
2021-11-15 15:19:58 +0100

    Include Cucumber tests (chapter 9)

---
+++ selenium-webdriver-testng/src/test/java/io/github/bonigarcia/webdriver/testng/ch9/bdd/LoginSteps.java
@@ -0,0 +1,64 @@
+/*
+ * (C) Copyright 2021 Boni Garcia (https://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.webdriver.testng.ch9.bdd;
+
+import java.util.NoSuchElementException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginSteps {
+
+    private WebDriver driver;
+
+    @When("I navigate to {string}")
+    public void iNavigateTo(String url) {
+        driver = WebDriverManager.chromedriver().create();
+        driver.get(url);
+    }
+
+    @And("I login with the username {string} and password {string}")
+    public void iLogin(String username, String password) {
+        driver.findElement(By.id("username")).sendKeys(username);
+        driver.findElement(By.id("password")).sendKeys(password);
+
+    }
+
+    @And("I click Submit")
+    public void iPressEnter() {
+        driver.findElement(By.cssSelector("button")).click();
+    }
+
+    @Then("I should be see the message {string}")
+    public void iShouldSee(String result) {
+        try {
+            driver.findElement(
+                    By.xpath("//*[contains(text(), '" + result + "')]"));
+        } catch (NoSuchElementException e) {
+            throw new AssertionError(
+                    "\"" + result + "\" not available in results");
+        } finally {
+            driver.quit();
+        }
+    }
+
+}
