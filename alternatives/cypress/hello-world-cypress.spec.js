1e414aebef0b06524ffea26e111d0e9e38e6e56b
Boni García <boni.garcia@uc3m.es>
2021-12-12 19:27:56 +0100

    Minor changes in alternative scripts

--- alternatives/cypress/hello-world-cypress.spec.js
+++ alternatives/cypress/hello-world-cypress.spec.js
@@ -2,11 +2,11 @@ describe('Hello World with Cypress', () => {
    it('Login in the practice site', () => {
       cy.visit('https://bonigarcia.dev/selenium-webdriver-java/login-form.html')
 	
-	  cy.get('#username').type('user')
-	  cy.get('#password').type('user')
-	  cy.contains('Submit').click()
-	  cy.contains('Login successful')
+      cy.get('#username').type('user')
+      cy.get('#password').type('user')
+      cy.contains('Submit').click()
+      cy.contains('Login successful')
 	  
-	  cy.screenshot("hello-world-cypres")
+      cy.screenshot("hello-world-cypres")
   })
 })
