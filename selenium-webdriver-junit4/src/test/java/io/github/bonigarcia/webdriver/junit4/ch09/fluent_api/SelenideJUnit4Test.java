0103c18247623bb2fa3cdc09ea30f3c9f42b4fc8
Boni García <boni.garcia@uc3m.es>
2021-12-13 13:19:34 +0100

    Include Selenide tests (chapter 9)

---
+++ selenium-webdriver-junit4/src/test/java/io/github/bonigarcia/webdriver/junit4/ch09/fluent_api/SelenideJUnit4Test.java
@@ -0,0 +1,40 @@
+/*
+ * (C) Copyright 2021 Boni Garcia (https://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.webdriver.junit4.ch09.fluent_api;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+public class SelenideJUnit4Test {
+
+    @Test
+    public void testSelenide() {
+        open("https://bonigarcia.dev/selenium-webdriver-java/login-form.html");
+
+        $(By.id("username")).val("user");
+        $(By.id("password")).val("user");
+        $(By.cssSelector("button")).pressEnter();
+        $(By.id("success")).shouldBe(visible)
+                .shouldHave(text("Login successful"));
+    }
+
+}
