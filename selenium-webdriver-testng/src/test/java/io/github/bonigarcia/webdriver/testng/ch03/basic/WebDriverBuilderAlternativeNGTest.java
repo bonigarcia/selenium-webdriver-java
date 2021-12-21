e177723c864ab9b725a789bf5be34114cc57ee5d
Boni García <boni.garcia@uc3m.es>
2021-12-21 17:06:39 +0100

    Revert "Disable tests using WebDriver builder with alternative"
    This reverts commit 8d58653b0f4b109f0110a169a8cfe3395ba52636.

--- selenium-webdriver-testng/src/test/java/io/github/bonigarcia/webdriver/testng/ch03/basic/WebDriverBuilderAlternativeNGTest.java
+++ selenium-webdriver-testng/src/test/java/io/github/bonigarcia/webdriver/testng/ch03/basic/WebDriverBuilderAlternativeNGTest.java
@@ -23,12 +23,10 @@ import org.openqa.selenium.safari.SafariOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-@Ignore("Skeleton test")
 public class WebDriverBuilderAlternativeNGTest {
 
     WebDriver driver;
