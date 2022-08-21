58257df740c539e9b6b14c2984017d2207850b68
Boni Garcia <boni.garcia@uc3m.es>
2022-08-21 21:47:55 +0200

    Include tests using monitoring features (through BrowserWatcher)

---
+++ selenium-webdriver-junit4/src/test/java/io/github/bonigarcia/webdriver/junit4/ch05/monitoring/DisplayLogsFirefoxJUnit4Test.java
@@ -0,0 +1,72 @@
+/*
+ * (C) Copyright 2022 Boni Garcia (https://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.webdriver.junit4.ch05.monitoring;
+
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DisplayLogsFirefoxJUnit4Test {
+
+    static final Logger log = getLogger(lookup().lookupClass());
+
+    WebDriverManager wdm = WebDriverManager.firefoxdriver().watchAndDisplay();
+    WebDriver driver;
+
+    @Before
+    public void setup() {
+        driver = wdm.create();
+    }
+
+    @After
+    public void teardown() throws InterruptedException {
+        // FIXME: pause for manual browser inspection
+        Thread.sleep(Duration.ofSeconds(3).toMillis());
+
+        driver.quit();
+    }
+
+    @Test
+    public void testDisplayLogsFirefox() {
+        driver.get(
+                "https://bonigarcia.dev/selenium-webdriver-java/console-logs.html");
+
+        List<Map<String, Object>> logMessages = wdm.getLogs();
+
+        assertThat(logMessages).hasSize(5);
+
+        logMessages.forEach(map -> log.debug("[{}] [{}] {}",
+                map.get("datetime"),
+                String.format("%1$-14s",
+                        map.get("source").toString().toUpperCase() + "."
+                                + map.get("type").toString().toUpperCase()),
+                map.get("message")));
+    }
+
+}
