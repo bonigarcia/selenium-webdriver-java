84eae4d39f9dd21b5abb27f09dc83ca0a41efdce
Boni Garcia <boni.garcia@uc3m.es>
2026-04-24 15:03:42 +0200

    Disable recording tests (broken as in Chrome 142+)

--- selenium-webdriver-testng/src/test/java/io/github/bonigarcia/webdriver/testng/ch05/monitoring/RecordChromeNGTest.java
+++ selenium-webdriver-testng/src/test/java/io/github/bonigarcia/webdriver/testng/ch05/monitoring/RecordChromeNGTest.java
@@ -30,10 +30,12 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+@Ignore("Broken in Chrome 142")
 public class RecordChromeNGTest {
 
     static final Logger log = getLogger(lookup().lookupClass());
